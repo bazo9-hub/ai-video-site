20e4b24ab17ac62c612763c07b14ec4e6e33f474
bazo9-hub <abdisey10@gmail.com>
2026-07-05 01:23:37 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,22 @@
+window.showApp = function() {
+    document.getElementById('landingPage').style.display = 'none';
+    document.getElementById('appBox').style.display = 'block';
+};
 
+window.signup = function() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(function() { document.getElementById("status").innerText = "✅ تم إنشاء الحساب"; })
+        .catch(function(e) { alert(e.message); });
+};
+
+window.login = function() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function() { document.getElementById("status").innerText = "✅ تم تسجيل الدخول"; })
+        .catch(function(e) { alert(e.message); });
+};
+
+window.logout = function() { auth.signOut(); };
