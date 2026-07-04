658ec4930d2cceb3e89e1022d948559d88fc931e
bazo9-hub <abdisey10@gmail.com>
2026-07-05 01:23:57 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBS70yDWhJy0Dyga40Uhi4L1m_4017Fe6c",
+    authDomain: "bazo-a36fe.firebaseapp.com",
+    databaseURL: "https://bazo-a36fe-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bazo-a36fe",
+    storageBucket: "bazo-a36fe.firebasestorage.app",
+    messagingSenderId: "91348188690",
+    appId: "1:91348188690:web:32769b1826e9aa0b8f22cb",
+    measurementId: "G-ES9JXGH44V"
+};
 
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+var db = firebase.firestore();
+var map, currentUser = null;
