db691d70ae2c0da0b6c901f18796766200a9dcec
bazo9-hub <abdisey10@gmail.com>
2026-07-05 01:28:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+// ===== مراقبة حالة المستخدم =====
+auth.onAuthStateChanged(function(user) {
+    currentUser = user;
+    if (user) {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("appBox").style.display = "block";
+        startMap(user);
+    } else {
+        document.getElementById("loginBox").style.display = "block";
+        document.getElementById("appBox").style.display = "none";
+    }
+});
