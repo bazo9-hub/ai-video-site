f5904563c285d8fe14d63710682f2fcbde37b241
bazo9-hub <abdisey10@gmail.com>
2026-07-09 04:26:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+// ===== مراقبة حالة المستخدم =====
 auth.onAuthStateChanged(function(user) {
     currentUser = user;
     if (user) {
         document.getElementById("loginBox").style.display = "none";
-        document.getElementById("mapPage").style.display = "block";
-        document.getElementById("profilePage").style.display = "none";
-        loadUserProfile();
+        document.getElementById("appBox").style.display = "block";
         startMap(user);
     } else {
         document.getElementById("loginBox").style.display = "block";
-        document.getElementById("mapPage").style.display = "none";
-        document.getElementById("profilePage").style.display = "none";
+        document.getElementById("appBox").style.display = "none";
     }
 });
